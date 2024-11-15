9c686ae51ac6a197ae16b902b7463f6b47513807
xKhan Dev <143298442+xKhanDev@users.noreply.github.com>
2024-11-15 14:59:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 // import Practice from './components/todo App/practice';
-// import Resturent from "./components/basics/resturent"
-// import Footer from './components/basics/Footer';
-// import UseState from "./components/hooks/UseState"
-// import UseEffect from "./components/hooks/UseEffect"
-// import UseReducer from './components/hooks/UseReducer';
-import Todo from './components/todo App/todo'
+import Resturent from "./components/basics/resturent"
+import Footer from './components/basics/Footer';
+import UseState from "./components/hooks/UseState"
+import UseEffect from "./components/hooks/UseEffect"
+import UseReducer from './components/hooks/UseReducer';
+// import Todo from './components/todo App/todo'
 
 const App = () => {
   return <>
-    {/* <Resturent />
-    <Footer />
+    <Resturent />
+     {/* <Footer />
     <UseState />
     <UseEffect />
     <UseReducer />
-    <Practice /> */}
-    <Todo />
+    <Practice /> 
+     <Todo /> */}
   </>;
 };
 
